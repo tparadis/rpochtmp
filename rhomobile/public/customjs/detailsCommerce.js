1e7e41384081ecc617d4c1757bcacfba88f8b427
Leo <leopoldo@hotmail.fr>
2016-02-13 10:29:50 +0100

    Correctif d'un bug qui n'affichait pas les bonnes sous categories pour un magasin*

--- rhomobile/public/customjs/detailsCommerce.js
+++ rhomobile/public/customjs/detailsCommerce.js
@@ -41,8 +41,8 @@ function afficheSpecificationMagasin()
 	var url = "http://rpoch.istic.univ-rennes1.fr/static/images/";
 	$("#imageCommerce").append("<img src='"+url+data.commerce.image+"' />")
 	$("#descrCommerce #contenu").append("<span class='titre' >\""+data.commerce.enseigne+"\"</span><br/>");
-	afficheTags(data.tag0, data.tag1, data.tag2);
-		
+	afficheTags(data.commerce.tag0, data.commerce.tag1, data.commerce.tag2);
+	
 	$("#descrCommerce #contenu").append("<br/><br/>");
 	$("#descrCommerce #contenu").append("<span class='others'>"+data.commerce.street_number+" "+data.commerce.route+"</span><br/>");
 	$("#descrCommerce #contenu").append("<span class='others'>Tel. "+data.commerce.phone_num+"</span><br/>");
@@ -65,21 +65,51 @@ function afficheTags(tag0, tag1, tag2)
 	
 	if(tag0 != null && tag0 >= 0)
 	{
-		temp = JSON.parse(localStorage["sscat"+tag0]);
-		t0 = temp[1];
+		t0 = findSSCat(tag0,0);
 	}
 	
 	if(tag1 != null && tag1 >= 0)
 	{
-		temp = JSON.parse(localStorage["sscat"+tag1]);
-		t1 = temp[1];
+		t1 = findSSCat(tag1,1);
 	}
 	if(tag2 != null && tag2 >= 0)
 	{
-		temp = JSON.parse(localStorage["sscat"+tag2]);
-		t2 = temp[1];
+		t2 = findSSCat(tag2,2);
 	}
+	
 	
 	$("#descrCommerce #contenu").append("<span class='descr'>"+t0+t1+t2+"</span>");
 	
 }
+
+//Converti un id de tag en nom !
+function findSSCat(id,t){
+	
+	var nbSS = parseInt(localStorage.getItem("nbSsCat"));
+	var i = 0;
+	var tmp;
+	while(i < nbSS)
+	{
+		tmp = JSON.parse(localStorage.getItem("sscat"+i));
+		if(tmp[0] == id)
+		{
+			if(t >0)
+				tmp[1] = ", "+tmp[1];
+			return tmp[1];
+		}
+		i++;
+	}
+	return "";
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
