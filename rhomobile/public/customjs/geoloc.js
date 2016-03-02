e5936ef72580555b3a5244cad60ead39723d9726
RPOCH <rpoch@Teodors-MacBook-Pro.local>
2016-03-02 18:53:43 +0100

    Geoloc refonctionne.

--- rhomobile/public/customjs/geoloc.js
+++ rhomobile/public/customjs/geoloc.js
@@ -8,7 +8,7 @@ $(document).ready(function(e){
 	
 	getLongLat();
 	timerId = setTimeout(validCoordinates, 1000);
-	window.setInterval(computeValues, 1200);
+	window.setInterval(computeValues, 3000);
 })
 
 
@@ -19,16 +19,27 @@ function getLongLat()
 {
 			
 	var v1 = $("geolatitude").html();
-	if (v1 == "Unavailable") {
+	if (v1 == "Unavailable" || v1 == "Reading") {
 		userlat = defaultPos[0];
 		userRealCoords = false;
 	}
+	else
+	{
+		userlat = parseFloat(v1);
+		userRealCoords = true;
+	}
 
-	var v1 = $("geolongitude").html();
-	if (v1 == "Unavailable") {
+	var v2 = $("geolongitude").html();
+	if (v2 == "Unavailable" || v2 == "Reading") {
 		userlong = defaultPos[1];
 		userRealCoords = false;
 	}
+	else
+	{
+		userlong = parseFloat(v2);
+		userRealCoords = true;
+	}
+	//alert("lat: "+defaultPos[0]+", "+userlat+" :: long: "+defaultPos[1]+", "+userlong);
 }
 
 
