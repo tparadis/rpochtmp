bd8e9c52d966e8e5c157e9d7e8af490e23fc0e2f
RPOCH <rpoch@Teodors-MacBook-Pro.local>
2016-03-16 14:53:29 +0100

    Correctif bouton HOME

--- rhomobile/public/customjs/homebtn.js
+++ rhomobile/public/customjs/homebtn.js
@@ -1,4 +1,4 @@
-$("geolatitude").after("<div id='homeBtn' style='position:absolute;bottom:10px;left:10px;width:30px;height:30px;background-color:black;'><img src='http://rpoch.istic.univ-rennes1.fr/static/images/homebtn.png' /></div>");
+$("body").append("<div id='homeBtn' style='position:absolute;bottom:10px;left:10px;width:30px;height:30px;background-color:black;z-index:9999;'><img src='http://rpoch.istic.univ-rennes1.fr/static/images/homebtn.png' /></div>");
 $("#homeBtn").on('click',function(){
 	Rho.WebView.navigate("/app/");
 });
