43fc5871c1cbef2082ab2eebe286ffb0f72dcc6a
RPOCH <rpoch@Teodors-MacBook-Pro.local>
2016-05-03 09:14:54 +0200

    panneau des langues

--- rhomobile/app/application.rb
+++ rhomobile/app/application.rb
@@ -4,9 +4,13 @@ class AppApplication < Rho::RhoApplication
   def initialize
     # Tab items are loaded left->right, @tabs[0] is leftmost tab in the tab-bar
     # Super must be called *after* settings @tabs!
+    
+    
     @tabs = nil
+    
+    
     #To remove default toolbar uncomment next line:
-    @@toolbar = nil
+    #@@toolbar = nil
     super
 
 
