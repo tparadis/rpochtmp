303980c71e9f34bbdd0ee5fe244e9d7fa124aff2
OUAIRY Léopold <ouairy@ccir.priv>
2016-06-21 15:44:36 +0200

    correctif

--- rhomobile/public/customjs/startup.js
+++ rhomobile/public/customjs/startup.js
@@ -9,11 +9,13 @@ $(document).ready(function(){
 		{
 			var fichier = new Rho.RhoFile(Rho.RhoFile.join(Rho.Application.userFolder, 'save.txt'), Rho.RhoFile.OPEN_FOR_READ_WRITE);
 			fichier.write("distMax:6")
+			fichier.close();
 		}
 		if(!Rho.RhoFile.exists(Rho.RhoFile.join(Rho.Application.userFolder, 'firsttime.txt')))
 		{
 			var fichier = new Rho.RhoFile(Rho.RhoFile.join(Rho.Application.userFolder, 'firsttime.txt'), Rho.RhoFile.OPEN_FOR_READ_WRITE);
 			fichier.write("[1,1,1,1]")
+			fichier.close();
 		}
 		var filename = Rho.RhoFile.join(Rho.Application.userFolder, 'save.txt') // build the path
 		var contents = Rho.RhoFile.read(filename) // read the file into a variable
